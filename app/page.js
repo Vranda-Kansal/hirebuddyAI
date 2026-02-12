b4ef4b16d0a34b389b8486bd1b78aa051fe0b30c
VrandaEducate <vranda@educateapp.in>
2026-02-12 14:48:11 +0530

    add shadcn/ui in my project

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
     return (
-        <h1>Hello</h1>
+        <div>
+            <Button>Click me</Button>
+        </div>
     )
 }
