d151d259f780f459e91723a5c8e4d77d7a19bf89
Vranda-Kansal <itzvaniaggarwal12@gmail.com>
2026-02-13 17:41:05 +0530

    create the project title

---
+++ app/_components/ProjectTitle.jsx
@@ -0,0 +1,8 @@
+export default function ProjectTitle() {
+    return (
+        <div className="flex flex-col items-center text-4xl font-bold min-h-[100px] justify-between text-center">
+            <div className="flex gap-3"><span className="text-[#625df6]">FIX:</span> <span>Feeling Anxiety?</span></div>
+            <div className="flex gap-2"><span>Say,</span><span className="text-[#625df6]">BYE!</span></div>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import FeatureCard from "@/app/_components/FeatureCard";
+import ProjectTitle from "@/app/_components/ProjectTitle";
 
 export default function Home() {
     return (
         <div>
+            <ProjectTitle/>
             <div className="w-full flex justify-evenly my-[3rem]">
                 <FeatureCard/>
                 <FeatureCard/>
