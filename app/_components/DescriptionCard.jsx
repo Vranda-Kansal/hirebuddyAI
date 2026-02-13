03fb369f89f1501467cc047f80f1a3069aa67d78
Vranda-Kansal <itzvaniaggarwal12@gmail.com>
2026-02-13 19:43:15 +0530

    shadow to both cards

--- app/_components/DescriptionCard.jsx
+++ app/_components/DescriptionCard.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 
-export default function DescriptionCard(){
+export default function DescriptionCard() {
     return (
-        <ul className="bg-[#1f2937] text-white font-medium font-mono rounded-xl flex-1 p-3 flex flex-col justify-center gap-2 min-h-[220px]">
+        <ul className="bg-[#1f2937] text-white font-medium font-mono rounded-xl flex-1 p-3 flex flex-col justify-center gap-2 min-h-[220px] shadow-[8px_0_0px_#625df6]
+">
 
-                <li>You will able to speak fluently</li>
-                <li>daily pratice with the question</li>
-                <li>Feedback at the end</li>
-                <li>All type question based on Resume</li>
+            <li>You will able to speak fluently</li>
+            <li>daily pratice with the question</li>
+            <li>Feedback at the end</li>
+            <li>All type question based on Resume</li>
         </ul>
     )
 }
